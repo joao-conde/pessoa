66e1dadf04f393d87e64cdf0ee258b60e30a23a5
João Conde <16060539+joao-conde@users.noreply.github.com>
2025-03-21 08:24:11 +0000

    refactor: remove override logic

--- pessoa/src/identity.rs
+++ pessoa/src/identity.rs
@@ -1,5 +1,4 @@
 use crate::{fake, faking::*};
-use rand::seq::IndexedRandom;
 use serde::Serialize;
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize)]
@@ -17,11 +16,14 @@ pub struct Identity {
 
 impl Identity {
     pub fn builder() -> IdentityBuilder {
-        IdentityBuilder {
+        IdentityBuilder::default()
+    }
+}
+
+impl Default for IdentityBuilder {
+    fn default() -> Self {
+        Self {
             locale: Locale::EnUs,
-            addresses: None,
-            phones: None,
-            emails: None,
         }
     }
 }
@@ -48,92 +50,46 @@ pub struct Job {
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct IdentityBuilder {
     locale: Locale,
-    addresses: Option<Vec<Address>>,
-    phones: Option<Vec<String>>,
-    emails: Option<Vec<String>>,
 }
 
 impl IdentityBuilder {
     const PASSWORD_LENGTH: usize = 25;
 
-    pub fn with_locale(mut self, locale: Locale) -> Self {
-        self.locale = locale;
-        self
-    }
-
-    pub fn address_from(mut self, addresses: Vec<Address>) -> Self {
-        self.addresses = Some(addresses);
-        self
-    }
-
-    pub fn phone_from(mut self, phones: Vec<String>) -> Self {
-        self.phones = Some(phones);
-        self
-    }
-
-    pub fn email_from(mut self, emails: Vec<String>) -> Self {
-        self.emails = Some(emails);
-        self
-    }
-
     pub fn build(&self) -> Identity {
         let first_name = fake!(FirstName, self.locale);
         let last_name = fake!(LastName, self.locale);
+        let country = fake!(CountryName, self.locale);
+        let country_code = fake!(CountryCode, self.locale);
+        let state = fake!(StateName, self.locale);
+        let state_code = fake!(StateAbbr, self.locale);
+        let city = fake!(CityName, self.locale);
+        let street = fake!(StreetName, self.locale);
+        let zip_code = fake!(ZipCode, self.locale);
+        let house_number = fake!(BuildingNumber, self.locale);
+        let address = Address {
+            country,
+            country_code,
+            state,
+            state_code,
+            city,
+            street,
+            postal_code: zip_code,
+            house_number,
+        };
+        let phone = fake!(CellNumber, self.locale);
+        let email = fake!(FreeEmail, self.locale);
         let username = fake!(Username, self.locale);
         let password = fake!(
             Password,
             self.locale,
             Self::PASSWORD_LENGTH..Self::PASSWORD_LENGTH + 1
         );
-
         let job = Job {
             company: fake!(CompanyName, self.locale),
             industry: fake!(Industry, self.locale),
             role: fake!(Profession, self.locale),
         };
-
         let credit_card = fake!(CreditCardNumber, self.locale);
-
-        let address = self
-            .addresses
-            .as_ref()
-            .and_then(|addresses| addresses.choose(&mut rand::rng()))
-            .cloned()
-            .unwrap_or_else(|| {
-                let country = fake!(CountryName, self.locale);
-                let country_code = fake!(CountryCode, self.locale);
-                let state = fake!(StateName, self.locale);
-                let state_code = fake!(StateAbbr, self.locale);
-                let city = fake!(CityName, self.locale);
-                let street = fake!(StreetName, self.locale);
-                let zip_code = fake!(ZipCode, self.locale);
-                let house_number = fake!(BuildingNumber, self.locale);
-                Address {
-                    country,
-                    country_code,
-                    state,
-                    state_code,
-                    city,
-                    street,
-                    postal_code: zip_code,
-                    house_number,
-                }
-            });
-
-        let phone = self
-            .phones
-            .as_ref()
-            .and_then(|phones| phones.choose(&mut rand::rng()))
-            .cloned()
-            .unwrap_or_else(|| fake!(CellNumber, self.locale));
-
-        let email = self
-            .emails
-            .as_ref()
-            .and_then(|emails| emails.choose(&mut rand::rng()))
-            .cloned()
-            .unwrap_or_else(|| fake!(FreeEmail, self.locale));
-
         Identity {
             first_name,
             last_name,
@@ -145,5 +101,10 @@ impl IdentityBuilder {
             job,
             credit_card,
         }
+    }
+
+    pub fn with_locale(mut self, locale: Locale) -> Self {
+        self.locale = locale;
+        self
     }
 }
--- pessoa/tests/mod.rs
+++ pessoa/tests/mod.rs
@@ -1,5 +1,5 @@
 use fake_rs::locales::{Data, EN, PT_PT};
-use pessoa::{Address, Identity, Locale};
+use pessoa::{Identity, Locale};
 
 #[test]
 fn build_identity_with_defaults() {
@@ -22,64 +22,4 @@ fn build_multiple_identities_same_builder() {
 fn build_identity_with_specific_locale() {
     let identity = Identity::builder().with_locale(Locale::PtPt).build();
     assert!(PT_PT::NAME_FIRST_NAME.contains(&identity.first_name.as_str()));
-}
-
-#[test]
-fn build_identity_with_addresses() {
-    let addresses = vec![
-        Address {
-            country: "Portugal".into(),
-            country_code: "PT".into(),
-            state: Some("Porto".into()),
-            state_code: None,
-            city: "Porto".into(),
-            street: "Rua Sá da Bandeira".into(),
-            postal_code: "4000-437".into(),
-            house_number: Some("605".into()),
-        },
-        Address {
-            country: "Portugal".into(),
-            country_code: "PT".into(),
-            state: Some("Porto".into()),
-            state_code: None,
-            city: "Porto".into(),
-            street: "Av. de Fernão de Magalhães".into(),
-            postal_code: "4350-171".into(),
-            house_number: Some("1776".into()),
-        },
-        Address {
-            country: "Portugal".into(),
-            country_code: "PT".into(),
-            state: Some("Porto".into()),
-            state_code: None,
-            city: "Porto".into(),
-            street: "Avenida da Boavista".into(),
-            postal_code: "4100-119".into(),
-            house_number: Some("2674".into()),
-        },
-    ];
-    let identity = Identity::builder().address_from(addresses.clone()).build();
-    assert!(addresses.contains(&identity.address));
-}
-
-#[test]
-fn build_identity_with_emails() {
-    let emails = vec![
-        "pessoa@gmail.com".into(),
-        "pessoa@hotmail.com".into(),
-        "johndoe@gmail.com".into(),
-    ];
-    let identity = Identity::builder().email_from(emails.clone()).build();
-    assert!(emails.contains(&identity.email));
-}
-
-#[test]
-fn build_identity_with_phones() {
-    let phones = vec![
-        "919999999".into(),
-        "+351 999 999 999".into(),
-        "964075554".into(),
-    ];
-    let identity = Identity::builder().phone_from(phones.clone()).build();
-    assert!(phones.contains(&identity.phone));
 }
