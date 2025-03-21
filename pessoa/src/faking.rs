cdcfb323e9f12ec62ded83638f2f8f9d2b5c7705
João Conde <16060539+joao-conde@users.noreply.github.com>
2025-03-21 08:39:49 +0000

    feat: all locales available by fakers

--- pessoa/src/faking.rs
+++ pessoa/src/faking.rs
@@ -13,6 +13,12 @@ pub enum Locale {
     EnUs,
     PtPt,
     PtBr,
+    FrFr,
+    ZhTw,
+    ZhCn,
+    JaJp,
+    ArSa,
+    DeDe,
 }
 
 #[macro_export]
@@ -22,6 +28,12 @@ macro_rules! fake {
             Locale::EnUs => $faker(::fake_rs::locales::EN, $($opt),*).fake(),
             Locale::PtPt => $faker(::fake_rs::locales::PT_PT, $($opt),*).fake(),
             Locale::PtBr => $faker(::fake_rs::locales::PT_BR, $($opt),*).fake(),
+            Locale::FrFr => $faker(::fake_rs::locales::FR_FR, $($opt),*).fake(),
+            Locale::ZhTw => $faker(::fake_rs::locales::ZH_TW, $($opt),*).fake(),
+            Locale::ZhCn => $faker(::fake_rs::locales::ZH_CN, $($opt),*).fake(),
+            Locale::JaJp => $faker(::fake_rs::locales::JA_JP, $($opt),*).fake(),
+            Locale::ArSa => $faker(::fake_rs::locales::AR_SA, $($opt),*).fake(),
+            Locale::DeDe => $faker(::fake_rs::locales::DE_DE, $($opt),*).fake(),
         }
     };
 }
--- pessoa/tests/mod.rs
+++ pessoa/tests/mod.rs
@@ -1,4 +1,4 @@
-use fake_rs::locales::{Data, EN, PT_PT};
+use fake_rs::locales::{AR_SA, DE_DE, Data, EN, FR_FR, JA_JP, PT_BR, PT_PT, ZH_CN, ZH_TW};
 use pessoa::{Identity, Locale};
 
 #[test]
@@ -11,7 +11,55 @@ fn build_identity_with_defaults() {
 }
 
 #[test]
-fn build_identity_with_specific_locale() {
+fn build_identity_with_en_us_locale() {
+    let identity = Identity::with_locale(Locale::EnUs);
+    assert!(EN::NAME_FIRST_NAME.contains(&identity.first_name.as_str()));
+}
+
+#[test]
+fn build_identity_with_pt_pt_locale() {
     let identity = Identity::with_locale(Locale::PtPt);
     assert!(PT_PT::NAME_FIRST_NAME.contains(&identity.first_name.as_str()));
+}
+
+#[test]
+fn build_identity_with_pt_br_locale() {
+    let identity = Identity::with_locale(Locale::PtBr);
+    assert!(PT_BR::NAME_FIRST_NAME.contains(&identity.first_name.as_str()));
+}
+
+#[test]
+fn build_identity_with_fr_fr_locale() {
+    let identity = Identity::with_locale(Locale::FrFr);
+    assert!(FR_FR::NAME_FIRST_NAME.contains(&identity.first_name.as_str()));
+}
+
+#[test]
+fn build_identity_with_zh_tw_locale() {
+    let identity = Identity::with_locale(Locale::ZhTw);
+    assert!(ZH_TW::NAME_FIRST_NAME.contains(&identity.first_name.as_str()));
+}
+
+#[test]
+fn build_identity_with_zh_cn_locale() {
+    let identity = Identity::with_locale(Locale::ZhCn);
+    assert!(ZH_CN::NAME_FIRST_NAME.contains(&identity.first_name.as_str()));
+}
+
+#[test]
+fn build_identity_with_ja_jp_locale() {
+    let identity = Identity::with_locale(Locale::JaJp);
+    assert!(JA_JP::NAME_FIRST_NAME.contains(&identity.first_name.as_str()));
+}
+
+#[test]
+fn build_identity_with_ar_sa_locale() {
+    let identity = Identity::with_locale(Locale::ArSa);
+    assert!(AR_SA::NAME_FIRST_NAME.contains(&identity.first_name.as_str()));
+}
+
+#[test]
+fn build_identity_with_de_de_locale() {
+    let identity = Identity::with_locale(Locale::DeDe);
+    assert!(DE_DE::NAME_FIRST_NAME.contains(&identity.first_name.as_str()));
 }
